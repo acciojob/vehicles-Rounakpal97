70e327b5c6092a411c56056d526c6f682e0a1a73
Rounakpal97 <rounakpal97@gmail.com>
2025-08-16 11:34:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,41 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+
+        // ---------- Test Vehicle ----------
+        Vehicle v = new Vehicle("Generic Vehicle");
+        v.move(30, 45);
+        v.steer(15);
+        v.stop();
+        System.out.println("Vehicle name: " + v.getName());
+        System.out.println("Current Speed: " + v.getCurrentSpeed());
+        System.out.println("Current Direction: " + v.getCurrentDirection());
+
+        System.out.println("-----------------------------");
+
+        // ---------- Test Car ----------
+        Car car = new Car("Sedan", 4, 4, 5, true, "Petrol", 5);
+        car.changeGear(2);
+        car.changeSpeed(60, 90);
+
+        System.out.println("-----------------------------");
+
+        // ---------- Test F1 ----------
+        F1 ferrari = new F1("Ferrari", true);
+        ferrari.move(50, 0);
+        ferrari.accelerate(60);   // increase speed and change gear
+        ferrari.accelerate(200);  // push to higher gear
+        ferrari.accelerate(-100); // slow down
+
+        System.out.println("-----------------------------");
+
+        // ---------- Test Boat ----------
+        Boat yacht = new Boat("Yacht", 40);
+        System.out.println("Boat Name: " + yacht.getVehicleName());
+        System.out.println("Boat Capacity: " + yacht.getVehicleCapacity());
+
+        Boat fishingBoat = new Boat("Fishing Boat", 10);
+        System.out.println("Boat Name: " + fishingBoat.getVehicleName());
+        System.out.println("Boat Capacity: " + fishingBoat.getVehicleCapacity());
     }
 }
