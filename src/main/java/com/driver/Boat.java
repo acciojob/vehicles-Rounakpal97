4b31dc6f03a65d846b7b6e4b8092de83306d9a9b
Rounakpal97 <rounakpal97@gmail.com>
2025-08-16 12:01:00 +0530

    Update Boat.java

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,19 +1,16 @@
+// Boat.java
 package com.driver;
 
-public class Boat implements WaterVehicle {
-    private String name;
+public class Boat extends Vehicle {
     private int capacity;
+    private String color;
 
-    public Boat(String name, int capacity) {
-        this.name = name;
+    public Boat(int capacity, String color) {
+        super();
         this.capacity = capacity;
+        this.color = color;
     }
 
-    public String getVehicleName() {
-        return name;
-    }
-
-    public int getVehicleCapacity() {
-        return capacity;
-    }
+    public int getCapacity() { return capacity; }
+    public String getColor() { return color; }
 }
