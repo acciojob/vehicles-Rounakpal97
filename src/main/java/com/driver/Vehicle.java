89ba86dca778bdccc8d657718025b1a748216127
Rounakpal97 <rounakpal97@gmail.com>
2025-08-16 12:00:13 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -1,35 +1,33 @@
+// Vehicle.java
 package com.driver;
 
 public class Vehicle {
-    private String name;
-    private int currentSpeed;
-    private int currentDirection;
+    private int speed;
+    private int direction;
 
-    public Vehicle(String name) {
-        this.name = name;
-        this.currentSpeed = 0;
-        this.currentDirection = 0;
+    public Vehicle() {
+        this.speed = 0;
+        this.direction = 0;
     }
 
-    public void steer(int direction) {
-        this.currentDirection += direction;
-        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
+    public void changeSpeed(int speed, int direction) {
+        this.speed = speed;
+        this.direction = direction;
+        System.out.println("changeSpeed method called - The speed is changed to: " + speed + ", and the direction is changed to: " + direction + " degrees");
     }
 
     public void move(int speed, int direction) {
-        this.currentSpeed = speed;
-        this.currentDirection = direction;
-        System.out.println("move method called - The speed is changed to: " + currentSpeed +
-                ", and the direction is changed to: " + currentDirection + " degrees");
+        changeSpeed(speed, direction);
+        System.out.println("move method called - The speed is changed to: " + speed + ", and the direction is changed to: " + direction + " degrees");
+    }
+
+    public void steer(int direction) {
+        this.direction = direction;
+        System.out.println("steer method called - The direction is changed to: " + direction + " degrees");
     }
 
     public void stop() {
-        this.currentSpeed = 0;
+        this.speed = 0;
         System.out.println("stop method called - The vehicle is stopped");
     }
-
-    // Getters
-    public int getCurrentSpeed() { return currentSpeed; }
-    public int getCurrentDirection() { return currentDirection; }
-    public String getName() { return name; }
 }
