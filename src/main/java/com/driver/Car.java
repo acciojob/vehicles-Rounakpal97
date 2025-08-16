1bfecfab9177ea252289707b0724bd780117c304
Rounakpal97 <rounakpal97@gmail.com>
2025-08-16 12:00:32 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,40 +1,35 @@
+// Car.java
 package com.driver;
 
 public class Car extends Vehicle {
     private int wheels;
-    private String type;
     private int doors;
     private int gears;
+    private int seats;
     private boolean isManual;
     private int currentGear;
-    private int seats;
+    private String type;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        super(name);
+    public Car(int wheels, int doors, int gears, int seats, boolean isManual, String type) {
+        super();
         this.wheels = wheels;
         this.doors = doors;
         this.gears = gears;
+        this.seats = seats;
         this.isManual = isManual;
         this.type = type;
-        this.seats = seats;
         this.currentGear = 1;
     }
 
-    public void changeGear(int newGear) {
-        this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
-    }
-
-    public void changeSpeed(int newSpeed, int newDirection) {
-        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
-    }
-
-    // Getters
+    public int getWheels() { return wheels; }
     public int getDoors() { return doors; }
     public int getGears() { return gears; }
-    public int getWheels() { return wheels; }
+    public int getSeats() { return seats; }
     public boolean isManual() { return isManual; }
     public String getType() { return type; }
-    public int getSeats() { return seats; }
-    public int getCurrentGear() { return currentGear; }
+
+    public void changeGear(int gear) {
+        this.currentGear = gear;
+        System.out.println("changeGear method called - The gear is changed to: " + gear);
+    }
 }
