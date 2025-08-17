4f64bcaf6af30578f76ece05a56721c13eedc34c
Rounakpal97 <rounakpal97@gmail.com>
2025-08-17 17:07:01 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -2,33 +2,41 @@ package com.driver;
 
 public class Car extends Vehicle {
     private int numberOfDoors;
-    private boolean isConvertible;
+    private boolean isManual;
     private String model;
-    private int year;
+    private int seats;
 
-    // 7-argument constructor (the one tests expect)
-    public Car(String name, int maxSpeed, int mileage, int numberOfDoors, boolean isConvertible, String model, int year) {
-        super(name, maxSpeed, mileage);
+    // For gear/speed tracking
+    private int currentSpeed;
+    private int currentGear;
+
+    // Constructor with all args (as required by tests)
+    public Car(String name, int speed, int fuel, int numberOfDoors, boolean isManual, String model, int seats) {
+        super(name, speed, fuel);
         this.numberOfDoors = numberOfDoors;
-        this.isConvertible = isConvertible;
+        this.isManual = isManual;
         this.model = model;
-        this.year = year;
+        this.seats = seats;
+        this.currentGear = 1; // default
+        this.currentSpeed = 0;
+    }
+
+    // Methods expected in tests
+    public void changeGear(int gear) {
+        this.currentGear = gear;
+    }
+
+    public void changeSpeed(int speed, int gear) {
+        this.currentSpeed = speed;
+        this.currentGear = gear;
     }
 
     // Getters
-    public int getNumberOfDoors() {
-        return numberOfDoors;
+    public int getCurrentGear() {
+        return currentGear;
     }
 
-    public boolean isConvertible() {
-        return isConvertible;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getYear() {
-        return year;
+    public int getCurrentSpeed() {
+        return currentSpeed;
     }
 }
