9f747c45d22c5d32d0c91636f754bb5c8444e8a0
Rounakpal97 <rounakpal97@gmail.com>
2025-08-17 17:04:42 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,41 +1,34 @@
 package com.driver;
 
 public class Car extends Vehicle {
-    private int doors;
-    private int gears;
-    private boolean isManual;
-    private int currentGear;
-    private String type;
-    private int seats;
-    private int wheels;
+    private int numberOfDoors;
+    private boolean isConvertible;
+    private String model;
+    private int year;
 
-    public Car(String name, int doors, int gears, boolean isManual, String type, int seats) {
-        super(name);
-        this.doors = doors;
-        this.gears = gears;
-        this.isManual = isManual;
-        this.type = type;
-        this.seats = seats;
-        this.wheels = 4; // standard for cars
-        this.currentGear = 1;
+    // 7-argument constructor (the one tests expect)
+    public Car(String name, int maxSpeed, int mileage, int numberOfDoors, boolean isConvertible, String model, int year) {
+        super(name, maxSpeed, mileage);
+        this.numberOfDoors = numberOfDoors;
+        this.isConvertible = isConvertible;
+        this.model = model;
+        this.year = year;
     }
 
-    public void changeGear(int gear) {
-        if (gear >= 1 && gear <= gears) {
-            this.currentGear = gear;
-        }
+    // Getters
+    public int getNumberOfDoors() {
+        return numberOfDoors;
     }
 
-    public void changeSpeed(int speed, int direction) {
-        move(speed, direction);
+    public boolean isConvertible() {
+        return isConvertible;
     }
 
-    // Getters for tests
-    public int getDoors() { return doors; }
-    public int getGears() { return gears; }
-    public boolean isManual() { return isManual; }
-    public int getCurrentGear() { return currentGear; }
-    public String getType() { return type; }
-    public int getSeats() { return seats; }
-    public int getWheels() { return wheels; }
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
