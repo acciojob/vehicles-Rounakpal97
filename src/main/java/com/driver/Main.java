22411b942b8fea2f96023e32b74cc772a5b8d807
Rounakpal97 <rounakpal97@gmail.com>
2025-08-17 16:59:34 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,13 @@
-// Main.java
 package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car(4, 4, 6, 5, true, "Sedan");
-        car.changeGear(1);
-        car.move(50, 0);
-        car.steer(90);
-        car.stop();
+        F1 f1 = new F1("Ferrari", true);
+        f1.accelerate(120);
 
-        F1 f1 = new F1(4, 0, 8, 1, false, "Formula");
-        f1.changeGear(1);
-        f1.move(100, 30);
-        f1.steer(75);
-        f1.stop();
+        Boat boat = new Boat("Yacht", 50);
 
-        Boat boat = new Boat(10, "Blue");
-        boat.move(20, 45);
-        boat.steer(90);
-        boat.stop();
+        System.out.println("Car: " + f1.getName() + ", Speed: " + f1.getCurrentSpeed() + ", Gear: " + f1.getCurrentGear());
+        System.out.println("Boat: " + boat.getVehicleName() + ", Capacity: " + boat.getCapacity());
     }
 }
