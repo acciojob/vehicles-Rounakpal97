b149859600eec2b1a66d8aa2c23c4eb62b45a909
Rounakpal97 <rounakpal97@gmail.com>
2025-08-17 17:11:47 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,13 +2,29 @@ package com.driver;
 
 public class F1 extends Car {
 
-    // Match Car constructor (7 parameters!)
-    public F1(String name, int speed, int fuel, int numberOfDoors, boolean isManual, String model, int seats) {
-        super(name, speed, fuel, numberOfDoors, isManual, model, seats);
+    public F1(String name, boolean isManual) {
+        //Use arbitrary values for parameters which are not mentioned
     }
 
-    // Extra methods for racing
-    public void accelerate(int speedChange, int gearChange) {
-        changeSpeed(getCurrentSpeed() + speedChange, gearChange);
+    public void accelerate(int rate){
+        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        /**
+         * speed 0: gear 1
+         * speed 1-50: gear 1
+         * speed 51-100: gear 2
+         * speed 101-150: gear 3
+         * speed 151-200: gear 4
+         * speed 201-250: gear 5
+         * speed more than 250: gear 6
+         */
+
+        if(newSpeed == 0) {
+            //Stop the car, set gear as 1
+        }
+        //for all other cases, change the gear accordingly
+
+        if(newSpeed > 0) {
+            changeSpeed(newSpeed, getCurrentDirection());
+        }
     }
 }
